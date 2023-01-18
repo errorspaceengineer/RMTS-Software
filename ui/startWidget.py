3ddf30175a8bd544247e0dc1186630811b42b007
Han Duong <114118476+errorspaceengineer@users.noreply.github.com>
2023-01-18 17:52:05 -0600

    Update startWidget.py

--- ui/startWidget.py
+++ ui/startWidget.py
@@ -50,7 +50,7 @@ class StartWidget(QWidget):
         logo = QtSvg.QSvgWidget()
         self.ui.widgetLogo.setLayout(QVBoxLayout())
         self.ui.widgetLogo.layout().addWidget(logo)
-        logo.load(os.path.join(os.path.dirname(sys.argv[0]), 'resources/logo_large.svg'))
+        logo.load(os.path.join(os.path.dirname(sys.argv[0]), 'resources/ITR_logo.png'))
 
     def showSavedResultsPressed(self):
         path = QFileDialog.getOpenFileName(None, 'Load FIRE', '', 'Firing Data File (*.fire)')[0]
